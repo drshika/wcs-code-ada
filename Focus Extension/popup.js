f98586859072f0cbc1ac3faac0920e7d65405b17
Athena <athenafung1@gmail.com>
2020-10-24 14:38:25 -0700

    add buttons

--- Focus Extension/popup.js
+++ Focus Extension/popup.js
@@ -1,12 +1,28 @@
 function enterTeacherInputs() {
-	var webpage = document.getElementById("link");
-	var interval = document.getElementById("timing");
+	var webpage = document.getElementsById("link").value;
+	var interval = document.getElementById("timing").value;
 }
 
 function enterQuestions() {
-	var question = document.getElementById("question");
+	var question = document.getElementById("question").value;
   	
   	/*question.setAttribute("type", "text");
   	question.setAttribute("value", "Hello World!");
   	document.body.appendChild(question);*/
 }
+
+function printInputValue() {
+	var input = document.getElementById("link").value;
+	document.write(input)
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	var newQButton = document.getElementsById('newButton');
+	newButton.addEventListener('click', appendButton() {
+		var newInputBox = document.createElement('moreQuestionFields');
+		newInputBox.type = "text";
+		document.getElementById("moreQuestionFields").appendChild(newInputBox);
+	});
+});
