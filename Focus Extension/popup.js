402c768489b9fe3e639e8c868171164daf35bac0
Athena <athenafung1@gmail.com>
2020-10-24 15:12:53 -0700

    fix input fields

--- Focus Extension/popup.js
+++ Focus Extension/popup.js
@@ -1,12 +1,26 @@
 function enterTeacherInputs() {
-	var webpage = document.getElementById("link");
-	var interval = document.getElementById("timing");
+	var webpage = document.getElementsById("link").value;
+	var interval = document.getElementById("timing").value;
 }
 
 function enterQuestions() {
-	var question = document.getElementById("question");
-  	
-  	/*question.setAttribute("type", "text");
-  	question.setAttribute("value", "Hello World!");
-  	document.body.appendChild(question);*/
+	var question = document.getElementById("question").value;
+}
+
+
+function printInputValue() {
+	var input = document.getElementById("link").value;
+	document.write(input);
+}
+
+
+addQuestionFields() {
+	var newInputBox = document.createElement("input");
+	newInputBox.setAttribute("type", "text");
+	document.body.appendChild(newInputBox);
+}
+
+var nb = document.getElementsById('newButton');
+if (nb) {
+	nb.addEventListener('click', addQuestionFields);
 }
