1799e3a983a5336d6e2dec820f9064ec80f2cfd2
Drshika A <67125579+drsxhi@users.noreply.github.com>
2020-10-24 15:32:02 -0500

    remove background.html

--- Focus Extension/background.js
+++ Focus Extension/background.js
@@ -1,22 +1,22 @@
-// chrome.alarms.onAlarm.addListener(activateAlarm(alarm));
+chrome.alarms.onAlarm.addListener(activateAlarm(alarm));
   
-//   chrome.notifications.onButtonClicked.addListener(function() {
-//     chrome.storage.sync.get(['minutes'], function(item) {
-//       chrome.browserAction.setBadgeText({text: 'ON'});
-//       chrome.alarms.create({delayInMinutes: item.minutes});
-//     });
-//   });
+    chrome.notifications.onButtonClicked.addListener(function() {
+    chrome.storage.sync.get(['minutes'], function(item) {
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    chrome.alarms.create({delayInMinutes: item.minutes});
+     });
+    });
 
-//   function activateAlarm(alarm){
-//     chrome.browserAction.setBadgeText({text: ''});
-//     chrome.notifications.create({
-//         type:     'basic',
-//         iconUrl:  'icon.png',
-//         title:    'Here\'s your questions',
-//         message:  'AAAAAA',
-//         buttons: [
-//           {title: 'Keep it Flowing.'}
-//         ],
-//         priority: 0});
-//   };
+   function activateAlarm(alarm){
+     chrome.browserAction.setBadgeText({text: ''});
+     chrome.notifications.create({
+         type:     'basic',
+         iconUrl:  'icon.png',
+         title:    'Here\'s your questions',
+         message:  'AAAAAA',
+         buttons: [
+           {title: 'Keep it Flowing.'}
+         ],
+         priority: 0});
+   };
   
